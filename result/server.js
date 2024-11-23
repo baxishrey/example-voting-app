602903685889b84e3235a48dd026b290f304545d
baxishrey <baxishrey@gmail.com>
2024-11-23 11:57:15 +0530

    Change TLD to .app to try with Service connect

--- result/server.js
+++ result/server.js
@@ -24,7 +24,7 @@ io.sockets.on('connection', function (socket) {
 });
 
 var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db.course/postgres'
+  connectionString: 'postgres://postgres:postgres@db.app/postgres'
 });
 
 async.retry(
